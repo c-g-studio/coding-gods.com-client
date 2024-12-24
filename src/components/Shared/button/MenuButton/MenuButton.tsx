6a77ddb999eb12e0094bc36598a1364410f90b4d
Alina <alia.nord.adventurer@gmail.com>
2024-12-24 14:45:02 +0100

    upd menu btn

--- src/components/Shared/button/MenuButton/MenuButton.tsx
+++ src/components/Shared/button/MenuButton/MenuButton.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {MenuIcon} from "../../../../../public/icons/MenuIcon";
+import { MenuIcon } from '../../../../../public/icons/MenuIcon';
 
-export const MenuButton = ( ) => {
+export const MenuButton = () => {
   return (
-    <button className='w-14 h-12 flex justify-center items-center'>
+    <button className="flex h-12 w-12 items-center justify-center gap-4 md:w-36 md:justify-end">
+      <span className="hidden md:inline-block">Меню</span>
       <MenuIcon width={28} height={28} />
     </button>
   );
 };
-
