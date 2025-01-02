019466928ba799f573362fac3a8ed2c13e68c32b
josserden <josserden@gmail.com>
2025-01-02 13:06:34 +0200

    feat: added library for animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -41,5 +41,5 @@ export default {
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require('@tailwindcss/forms'), require('tailwindcss-motion')],
 } satisfies Config;
