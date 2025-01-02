e6b29d74442df2557f48f5394c9c8c33ba1d036f
josserden <josserden@gmail.com>
2025-01-02 13:22:33 +0200

    feat: added base mock data

--- src/components/pages/home/DevelopmentStages/DevelopmentStages.tsx
+++ src/components/pages/home/DevelopmentStages/DevelopmentStages.tsx
@@ -1,11 +1,14 @@
+import { FC } from 'react';
+
 import { Section } from '@/components/shared/Section/Section';
 import { Typography } from '@/components/ui/Typography/Typography';
-import { FC } from 'react';
 
 export const DevelopmentStages: FC = () => {
   return (
     <Section>
-      <Typography variant="h2">Етапи розробки</Typography>
+      <Typography className="text-center" variant="h2">
+        Етапи розробки
+      </Typography>
 
       <div>
         {/*List of development stages*/}
