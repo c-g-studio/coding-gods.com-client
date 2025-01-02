3cc1a5a6d2e840d3c8b1c2b3ece92ca0f29a5360
josserden <josserden@gmail.com>
2025-01-02 16:36:32 +0200

    feat: added chart component

--- src/components/pages/home/DevelopmentStages/DevelopmentStages.tsx
+++ src/components/pages/home/DevelopmentStages/DevelopmentStages.tsx
@@ -1,38 +1,29 @@
-import { Icon } from '@/components/ui/Icon/Icon';
 import { FC } from 'react';
 
+import { DevelopmentChart } from '@/components/pages/home/DevelopmentStages/components/DevelopmentChart/DevelopmentChart';
+import { DevelopmentStagesList } from '@/components/pages/home/DevelopmentStages/components/DevelopmentStagesList';
 import { Section } from '@/components/shared/Section/Section';
 import { Typography } from '@/components/ui/Typography/Typography';
 
-import data from './data.json';
-
 export const DevelopmentStages: FC = () => {
-  // width="49" height="49"
   return (
     <Section>
-      <Typography className="text-center" variant="h2">
+      <Typography
+        className="motion-preset-expand text-center motion-delay-100"
+        variant="h2"
+      >
         Етапи розробки
       </Typography>
 
-      <div>
+      <div className="relative flex items-center gap-x-[262px]">
         {/*List of development stages*/}
-        <div>
-          <ul>
-            {data.map(stage => (
-              <li key={stage.type} className="text">
-                <Icon type={stage.type} width={49} height={49} />
-                <Typography variant="h3">{stage.title}</Typography>
-                <Typography>{stage.description}</Typography>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <DevelopmentStagesList />
 
         {/*Decor*/}
         <div></div>
 
-        {/*Subtitle*/}
-        <div></div>
+        {/*Chart*/}
+        <DevelopmentChart />
       </div>
     </Section>
   );
