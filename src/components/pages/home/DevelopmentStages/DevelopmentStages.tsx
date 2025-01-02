aececbb19c7d17ab6c9c9a7215b0dd219bc335b6
josserden <josserden@gmail.com>
2025-01-02 13:02:36 +0200

    feat: added base structure

---
+++ src/components/pages/home/DevelopmentStages/DevelopmentStages.tsx
@@ -0,0 +1,22 @@
+import { Section } from '@/components/shared/Section/Section';
+import { Typography } from '@/components/ui/Typography/Typography';
+import { FC } from 'react';
+
+export const DevelopmentStages: FC = () => {
+  return (
+    <Section>
+      <Typography variant="h2">Етапи розробки</Typography>
+
+      <div>
+        {/*List of development stages*/}
+        <div></div>
+
+        {/*Decor*/}
+        <div></div>
+
+        {/*Subtitle*/}
+        <div></div>
+      </div>
+    </Section>
+  );
+};
