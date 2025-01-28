1c0e6f72021614908b002cd91901b34552be9b1f
josserden <josserden@gmail.com>
2025-01-28 21:54:58 +0200

    feat: implement form validation in FooterForm using Zod and integrate translation

--- src/components/layout/Footer/FooterForm/FooterForm.tsx
+++ src/components/layout/Footer/FooterForm/FooterForm.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
+'use client';
 
-import { getTranslate } from '@/tolgee/server';
+import React from 'react';
+import { useTranslate } from '@tolgee/react';
+
 import { BaseForm } from '@/components/shared/FormElements/BaseForm';
 
 import styles from '../Footer.module.css';
+import { z } from 'zod';
 
-export const FooterForm = async () => {
-  const t = await getTranslate();
+const formSchema = z.object({
+  phone: z.string().min(10, {
+    message: 'Невірний формат номеру телефону',
+  }),
+});
+
+export const FooterForm = () => {
+  const { t } = useTranslate();
 
   return (
     <BaseForm
+      handler={async data => {
+        try {
+          alert(JSON.stringify(data, null, 2));
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      }}
+      schema={formSchema}
       className={`${styles.footerForm}`}
       legend={t('home.common.submitBtn')}
       fields={[
