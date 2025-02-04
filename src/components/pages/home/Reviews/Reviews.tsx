375ea3a6992e419e2977646db7958271109d52dc
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2025-02-05 00:04:50 +0200

    add slider

--- src/components/pages/home/Reviews/Reviews.tsx
+++ src/components/pages/home/Reviews/Reviews.tsx
@@ -4,8 +4,8 @@ import {Section} from "@/components/shared/Section/Section";
 import {ReviewsList} from "@/components/pages/home/Reviews/components/ReviewsList/ReviewsList";
 
 export const Reviews: FC = (): React.JSX.Element => {
-  return <Section>
-    <Typography variant="h2">
+  return <Section className="overflow-hidden lg:overflow-visible">
+    <Typography variant="h2" className="text-center ">
       Відгуки
     </Typography>
     <ReviewsList/>
--- src/components/pages/home/Reviews/components/ReviewsList/ReviewsList.tsx
+++ src/components/pages/home/Reviews/components/ReviewsList/ReviewsList.tsx
@@ -1,58 +1,94 @@
+'use client'
 import React, {FC} from "react";
 import {Card, Cards} from '@/components/ui/Card/Card';
 import Image from "next/image";
 import {Typography} from "@/components/ui/Typography/Typography";
+import {Swiper, SwiperSlide} from "swiper/react";
+import s from "./reviews.module.css"
+import 'swiper/css';
 
 const mockData = [
   {
+    id: "1",
     img: "/images/about-us/about-us-medium.jpg",
     title: "Vika",
-    review: "Its cool",
+    review: "Дякуємо за вашу службу. Результатом я задоволена. На всіх етапах розробки проукту відчувалась підтримка команди.",
     source: "Google Maps",
-    icon: "/images/globe.svg"
+    icon: "/globe.svg"
   },
   {
+    id: "2",
     img: "/images/about-us/about-us-medium.jpg",
     title: "Roma",
     review: "Всё супер",
     source: "Google Maps",
-    icon: "/images/globe.svg"
+    icon: "/globe.svg"
   },
   {
+    id: "3",
     img: "/images/about-us/about-us-medium.jpg",
     title: "Oksana",
     review: "Супер команда, оч помогли",
     source: "Google Maps",
-    icon: "/images/globe.svg"
+    icon: "/globe.svg"
   },
   {
+    id: "4",
     img: "/images/about-us/about-us-medium.jpg",
     title: "Dana",
     review: "Фуфлыжныики, запороли что-то. Онлифанс не работает",
     source: "Google Maps",
-    icon: "/images/globe.svg"
+    icon: "/globe.svg"
   }
 ]
+// className={`flex w-[220px] flex-col rounded-[20px] border border-zinc-200 border-opacity-20 px-4 pt-6 pb-[34px] md:w-[260px] md:px-[26px] max-w-[220px] mr-[50px] md:max-w-full ${s.backgroundGradient}`}>
+
 
 export const ReviewsList: FC = (): React.JSX.Element => {
-  return <Cards.Wrapper>
+  return <Swiper
+    slidesPerView={'auto'}
+    centeredSlides={true}
+    breakpoints={{
+      768: {
+        spaceBetween: 50,
+        slidesPerView: 1.25,
+        centeredSlides: false
+      },
+      1440: {
+        spaceBetween: 50,
+        centeredSlides: false,
+        slidesPerView: 2
+      }
+    }}
+    onSlideChange={() => console.log('slide change')}
+    onSwiper={(swiper) => console.log(swiper)}
+    className="w-full reviews_swiper_global"
+  >
     {mockData && mockData.map((mockDatum) => (
-      <Card.Item>
-        <Card.Header>
-          <Image src={mockDatum.img} alt="Зобарження користувача" width={50} height={50}/>
+      <SwiperSlide
+        key={mockDatum.id}
+        className={`rounded-[20px] border border-zinc-200 border-opacity-20 px-4 pt-6 pb-[34px] md:px-[26px] max-w-[220px] md:max-w-full h-[300px] md:h-[260px] ${s.backgroundGradient}`}>
+        <div className="overflow-hidden w-20 h-20 rounded-[50%] flex items-center justify-center mx-auto mb-4">
+          <Image src={mockDatum.img} alt="Зобарження користувача" width={80} height={80}
+                 className="object-cover rounded-[50%] w-20 h-20"/>
+        </div>
+        <div className="flex-grow flex flex-col justify-between">
+          <Typography variant="p" className="mb-4 text-[10px]/[1.8]">
+            {mockDatum.review}
+          </Typography>
           <div>
-            <Typography variant="p">
-              {mockDatum.review}
-            </Typography>
-            <Typography variant="h3">
+            <Typography variant="h3" className="mb-4 text-2xl/[1.71]">
               {mockDatum.title}
             </Typography>
-            <Typography variant="p">
-              {mockDatum.source}
-            </Typography>
+            <div className="flex gap-2 mt-auto">
+              <Typography variant="p" className="text-[10px]/[1.8]">
+                {mockDatum.source}
+              </Typography>
+              <Image src={mockDatum.icon} alt="Зобарження Користувача" width={20} height={20}/>
+            </div>
           </div>
-        </Card.Header>
-      </Card.Item>
+        </div>
+      </SwiperSlide>
     ))}
-  </Cards.Wrapper>
+  </Swiper>
 }
